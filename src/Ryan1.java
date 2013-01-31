04e536fdc8ae5ad7aee9d2024dd1222b133d814c
Ryan <Ryan@10.1.9.233>
2013-01-31 04:24:58 -0600

    First commit

---
+++ src/Ryan1.java
@@ -0,0 +1,26 @@
+/**
+ * This program demonstrates the use of basic variables to calculate a worker's gross pay
+ * 
+ * &author Ryan Hickman
+ * 
+ */
+
+public class Ryan1 
+{
+	public static void main(String [] args)
+	{
+		int grossPay;		// Gross pay
+		int hours = 40;		// Hours worked
+		int payRate = 25;	// Pay rate
+		
+		grossPay = hours * payRate;								//Calculates the worker's gross pay
+		System.out.println("Your grossPay is $" + grossPay);	//Displays the worker's gross pay on the screen
+				
+			
+		
+	}
+	
+	
+
+}
+
